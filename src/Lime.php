b1a69d2bbb4ffabcc5dc55088641388a03b4df47
hoanguyen <hoanguyen.it@imap.edu.vn>
2021-08-25 15:11:03 +0700

    Lime.php

---
+++ src/Lime.php
@@ -0,0 +1,35 @@
+<?php
+namespace Microservices;
+
+class Lime
+{
+
+    public function __construct() {
+		/* Remind to download and put files in https://github.com/weberhofer/jsonrpcphp at the good place */
+		$this->_rpcUrl="https://demo-qlcl.imap.edu.vn/index.php/admin/remotecontrol";
+		$this->_rpcUser="admin";
+		$this->_rpcPassword="hoanguyen";
+
+		$this->_lsJSONRPCClient = new \org\jsonrpcphp\jsonRPCClient($this->_rpcUrl);
+    }
+
+    public function sessionKey(){
+    	$lsJSONRPCClient = $this->_lsJSONRPCClient;
+		$sessionKey= $lsJSONRPCClient->get_session_key($this->_rpcUser, $this->_rpcPassword);
+		return $sessionKey;
+    }
+
+    public function lime($name){
+    	$lsJSONRPCClient = $this->_lsJSONRPCClient;
+    	$sessionKey = $this->sessionKey();
+    	$response = $lsJSONRPCClient->$name($sessionKey,null);
+    	return $response;
+    }
+
+    public function copy_survey($iSurveyID_org, $sNewname){
+        $lsJSONRPCClient = $this->_lsJSONRPCClient;
+        $sessionKey = $this->sessionKey();
+        $response = $lsJSONRPCClient->copy_survey($sessionKey, $iSurveyID_org, $sNewname);
+        return $response;
+    }
+}
