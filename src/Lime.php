fbcfd870a6b41664b6b0a6a29aaf980d7019e66f
hoanguyen <hoanguyen.it@imap.edu.vn>
2021-08-26 15:16:43 +0700

    .

--- src/Lime.php
+++ src/Lime.php
@@ -5,25 +5,25 @@ class Lime
 {
 
     public function __construct() {
-		/* Remind to download and put files in https://github.com/weberhofer/jsonrpcphp at the good place */
-		$this->_rpcUrl="https://demo-qlcl.imap.edu.vn/index.php/admin/remotecontrol";
-		$this->_rpcUser="admin";
-		$this->_rpcPassword="hoanguyen";
+        /* Remind to download and put files in https://github.com/weberhofer/jsonrpcphp at the good place */
+        $this->_rpcUrl="https://demo-qlcl.imap.edu.vn/index.php/admin/remotecontrol";
+        $this->_rpcUser="admin";
+        $this->_rpcPassword="hoanguyen";
 
-		$this->_lsJSONRPCClient = new \org\jsonrpcphp\jsonRPCClient($this->_rpcUrl);
+        $this->_lsJSONRPCClient = new \org\jsonrpcphp\jsonRPCClient($this->_rpcUrl);
     }
 
     public function sessionKey(){
-    	$lsJSONRPCClient = $this->_lsJSONRPCClient;
-		$sessionKey= $lsJSONRPCClient->get_session_key($this->_rpcUser, $this->_rpcPassword);
-		return $sessionKey;
+        $lsJSONRPCClient = $this->_lsJSONRPCClient;
+        $sessionKey= $lsJSONRPCClient->get_session_key($this->_rpcUser, $this->_rpcPassword);
+        return $sessionKey;
     }
 
     public function lime($name){
-    	$lsJSONRPCClient = $this->_lsJSONRPCClient;
-    	$sessionKey = $this->sessionKey();
-    	$response = $lsJSONRPCClient->$name($sessionKey,null);
-    	return $response;
+        $lsJSONRPCClient = $this->_lsJSONRPCClient;
+        $sessionKey = $this->sessionKey();
+        $response = $lsJSONRPCClient->$name($sessionKey,null);
+        return $response;
     }
 
     public function copy_survey($iSurveyID_org, $sNewname){
@@ -31,5 +31,12 @@ class Lime
         $sessionKey = $this->sessionKey();
         $response = $lsJSONRPCClient->copy_survey($sessionKey, $iSurveyID_org, $sNewname);
         return $response;
+    }
+
+    public function get_responses($iSurveyID, $params = array()){
+        $lsJSONRPCClient = $this->_lsJSONRPCClient;
+        $sessionKey = $this->sessionKey();
+        $response = $lsJSONRPCClient->export_responses($sessionKey, $iSurveyID, 'json');
+        return json_decode(base64_decode($response), TRUE);
     }
 }
