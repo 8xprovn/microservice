7309216a7cf72546607466b0f0a2833c524c94c4
hoanguyen <hoanguyen.it@imap.edu.vn>
2021-09-15 15:21:27 +0700

    bỏ create ticket

--- src/Pm.php
+++ src/Pm.php
@@ -7,7 +7,6 @@ class Pm
     protected $_url;
     public function __construct() {
         $this->_url = env('API_MICROSERVICE_URL').'/pm';
-        $this->_local_url = env('APP_URL').'/pm';
     }
 
     public function getTickets($params = array())
@@ -167,21 +166,4 @@ class Pm
         \Log::error($response->body());
         return false;
     }
-
-    public function createTicket($input)
-    {
-        $input = \Arr::only($input, ['name','priority','department_id','description']);
-        try {
-            $response = \Http::post($this->_local_url.'/api/ticket', $input);
-            if ($response->successful()) {
-                return $response->json();
-            }
-            \Log::error($response->body());
-        }catch (\Throwable $e){
-            $result =  ['status' => 'error', 'message' => $e->getMessage()];
-            return $result;
-        }
-        return false;
-    }
-
 }
