241869d406f6325e874b6c38575f9fb00ff35b75
hoanguyen004 <hoanguyen004@gmail.com>
2023-01-10 18:33:16 +0700

    Update Calls.php

--- src/models/CallCenter/Calls.php
+++ src/models/CallCenter/Calls.php
@@ -4,10 +4,11 @@ namespace Microservices\Models\CallCenter;
 
 use Illuminate\Support\Arr;
 
-class Calls extends \Models\Model
+class Calls extends \Microservices\Models\Model
 {
     protected $_url;
     public function __construct() {
         $this->_url = env('API_MICROSERVICE_URL_V2').'/call-center';
+        $this->prefix = "calls";
     }
 }
