865f3696a6461031917b51435327768de4f45a5c
hoanguyen004 <hoanguyen004@gmail.com>
2023-02-08 13:42:34 +0700

    Update ModelV2.php

--- src/models/ModelV2.php
+++ src/models/ModelV2.php
@@ -1,15 +1,24 @@
 <?php
-namespace Models;
+namespace Microservices\Models;
 
 abstract class ModelV2
 {
-    public function all($filters = [], $options = [])
+    public function all($params = [], $options = [])
     {
-        $q = ($filters) ? ['filter' => $filters] : '';  
+        $filter = [];
+        foreach($params as $k => $v){
+            if (is_null($v)) continue;
+            switch ($k) {
+                default:
+                    $filter[$k] = $v;
+                    break;
+            }
+        }
+        $q = ['filter' => $filter];
         $response = \Http::withToken(env('API_MICROSERVICE_TOKEN',''))->get($this->_url.'/'.$this->prefix, $q);
         if ($response->successful()) {
             return $response->json();
-        }
+        } 
         \Log::error($response->body());
         return false;
     }
