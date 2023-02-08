edd2931c3c5ef3b85f4db5db129e171b1a9a8bf6
hoanguyen004 <hoanguyen004@gmail.com>
2023-02-08 11:33:37 +0700

    Add files via upload

---
+++ src/models/Hr/Employees.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Models\Hr;
+
+use Illuminate\Support\Arr;
+
+class Employees extends \Models\ModelV2
+{
+    protected $_url;
+    public function __construct() {
+        $this->_url = env('API_MICROSERVICE_URL_V2').'/hr';
+    }
+    protected $prefix = 'employees';
+}
