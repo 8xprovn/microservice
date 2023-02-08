871f27ee487e53ddb4517384f98636224043812e
hoanguyen004 <hoanguyen004@gmail.com>
2023-02-08 16:15:54 +0700

    Update Employees.php

--- src/models/Hr/Employees.php
+++ src/models/Hr/Employees.php
@@ -7,9 +7,8 @@ use Illuminate\Support\Arr;
 class Employees extends \Microservices\models\Model
 {
     protected $_url;
-    protected $prefix;
     public function __construct() {
         $this->_url = env('API_MICROSERVICE_URL_V2').'/hr';
-        $this->prefix = 'employees';
     }
+    protected $prefix = 'employees';
 }
