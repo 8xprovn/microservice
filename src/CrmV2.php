3e06b23afba13c527fe79abba9ee59a2622b4069
hoanguyen004 <hoanguyen004@gmail.com>
2023-05-24 12:02:19 +0700

    Update CrmV2.php

--- src/CrmV2.php
+++ src/CrmV2.php
@@ -29,4 +29,26 @@ class CrmV2
         \Log::error($response->body());
         return false;
     }
+    
+    public function getContacts($params= array())
+    {
+        $whereArr = \Arr::only($params, ['filter', 'page', 'limit']);
+        $response = \Http::withToken(env('API_MICROSERVICE_TOKEN',''))->get($this->_url.'/contacts', $params);
+        if ($response->successful()) {
+             return $response->json();
+        }
+        \Log::error($response->body());
+        return false;
+    }
+    
+    public function findContact($phoneOrEmail) {
+        if (filter_var($phoneOrEmail, FILTER_VALIDATE_EMAIL)) {
+            $contacts = $this->getContacts(['filter' => ['email' => $phoneOrEmail]]);
+        }
+        else {
+            $phoneOrEmail = preg_replace('/^0/', '+84', $phoneOrEmail);
+            $contacts = $this->getContacts(['filter' => ['phone' => $phoneOrEmail]]);
+        }
+        return $contacts ? $contacts : [];
+    }
 }
