11c511ecd063b5f7ca967007189d7432d350a86c
hoanguyen004 <hoanguyen004@gmail.com>
2023-06-30 15:05:52 +0700

    Create TestsV2.php

---
+++ src/TestsV2.php
@@ -0,0 +1,22 @@
+<?php
+namespace Microservices;
+
+class TestsV2
+{
+    protected $_url;
+    protected $_hash_secret;
+    public function __construct() {
+        $this->_url = env('APP_URL').'/api';
+        $this->_hash_secret = env('TEST_HASH_SECRET');
+    }
+
+    public function getTestLogDetail($id)
+    {
+        $response = \Http::withToken(env('API_MICROSERVICE_TOKEN',''))->get($this->_url.'/test-logs/'.$id);
+        if ($response->successful()) {
+            return $response->json();
+        }
+        \Log::error($response->body());
+        return false;
+    }
+}
