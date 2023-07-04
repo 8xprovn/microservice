64abcb4fd1538cb13f116bb4bbd82a02cd65b32e
hoanguyen004 <hoanguyen004@gmail.com>
2023-07-04 18:48:02 +0700

    Update TestsV2.php

--- src/TestsV2.php
+++ src/TestsV2.php
@@ -10,6 +10,48 @@ class TestsV2
         $this->_hash_secret = env('TEST_HASH_SECRET');
     }
 
+    //Tests
+    public function getTests($params = array())
+    {
+        $whereArr = \Arr::only($params, ['filter','page','limit']);
+        $response = \Http::withToken(env('API_MICROSERVICE_TOKEN',''))->get($this->_url.'/tests',$params);
+        if ($response->successful()) {
+             return $response->json();
+        }
+        \Log::error($response->body());
+        return false;
+    }
+
+    public function getTestDetail($id){
+        $response = \Http::withToken(env('API_MICROSERVICE_TOKEN',''))->get($this->_url.'/tests/'.$id);
+        if ($response->successful()) {
+            return $response->json();
+        }
+        \Log::error($response->body());
+        return false;
+    }
+
+    //Questions
+    public function getQuestions($params = array())
+    {
+        $whereArr = \Arr::only($params, ['filter','page','limit']);
+        $response = \Http::withToken(env('API_MICROSERVICE_TOKEN',''))->get($this->_url.'/questions',$params);
+        if ($response->successful()) {
+             return $response->json();
+        }
+        \Log::error($response->body());
+        return false;
+    }
+
+    public function getQuestionDetail($id){
+        $response = \Http::withToken(env('API_MICROSERVICE_TOKEN',''))->get($this->_url.'/questions/'.$id);
+        if ($response->successful()) {
+            return $response->json();
+        }
+        \Log::error($response->body());
+        return false;
+    }
+
     public function getTestLogDetail($id)
     {
         $response = \Http::withToken(env('API_MICROSERVICE_TOKEN',''))->get($this->_url.'/test-logs/'.$id);
