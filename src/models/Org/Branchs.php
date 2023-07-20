2e2f418ebfacadb806c6c27ff193025e8cfda7e6
hoanguyen004 <hoanguyen004@gmail.com>
2023-07-20 14:54:45 +0700

    Update Branchs.php

--- src/models/Org/Branchs.php
+++ src/models/Org/Branchs.php
@@ -8,7 +8,7 @@ class Branchs extends \Microservices\models\Model
 {
     protected $_url;
     public function __construct($options = []) {
-        $this->_url = env('API_MICROSERVICE_URL_V2').'/org/branchs';
+        $this->_url = env('API_MICROSERVICE_URL_V2').'/org/branch';
         $this->setToken($options['token'] ?? 'system');
     }
 }
