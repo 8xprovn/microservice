a41eb974cd3607528f65a4349e925d6805339d76
hoanguyen004 <hoanguyen004@gmail.com>
2023-07-31 17:35:46 +0700

    Update Device.php

--- src/models/Hr/Device.php
+++ src/models/Hr/Device.php
@@ -11,4 +11,26 @@ class Device extends \Microservices\models\Model
         $this->_url = env('API_MICROSERVICE_URL_V2').'/hr/device';
         $this->setToken($options['token'] ?? 'system');
     }
+
+    public function storeFirst(array $params)
+    {
+        $params = \Arr::whereNotNull($params);
+        if (!empty($this->only['create'])) {
+            $params = \Arr::only($params, $this->only['create']);
+        }
+        if (!empty($this->dataDefault['create'])) {
+            $params = array_merge($this->dataDefault['create'], $params);
+        }
+        if (!empty($this->idAutoIncrement) && empty($params[$this->primaryKey])) {
+            $params[$this->primaryKey] = $this->getNextSequence($this->table);
+        }
+        $params['created_time'] = time();
+        $url = env('API_MICROSERVICE_URL_V2').'/hr/device/store-first';
+        $response = \Http::acceptJson()->withToken($this->access_token)->POST($url, $params);
+        if ($response->successful()) {
+            return $response->json();
+        } 
+        \Log::error($url . $response->body());
+        return false;
+    }
 }
