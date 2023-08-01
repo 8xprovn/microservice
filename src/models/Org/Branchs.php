2d15d949d39466408909649c599cac8daac6fc04
Namtq <quocnam@hanoicdc.com>
2023-08-01 14:43:35 +0700

    cache with org

--- src/models/Org/Branchs.php
+++ src/models/Org/Branchs.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Arr;
 class Branchs extends \Microservices\models\Model
 {
     protected $_url;
+    protected $is_cache = 1;
     public function __construct($options = []) {
         $this->_url = env('API_MICROSERVICE_URL_V2').'/org/branch';
         $this->setToken($options['token'] ?? 'system');
--- src/models/Org/Brands.php
+++ src/models/Org/Brands.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Arr;
 class Brands extends \Microservices\models\Model
 {
     protected $_url;
+    protected $is_cache = 1;
     public function __construct($options = []) {
         $this->_url = env('API_MICROSERVICE_URL_V2').'/org/brand';
         $this->setToken($options['token'] ?? 'system');
