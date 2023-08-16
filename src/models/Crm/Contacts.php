7d7f6e30bdd61cc4f7bc1ff8a1a39e945636f0cf
hoanguyen004 <hoanguyen004@gmail.com>
2023-08-16 10:43:58 +0700

    Update Contacts.php

--- src/models/Crm/Contacts.php
+++ src/models/Crm/Contacts.php
@@ -15,11 +15,11 @@ class Contacts extends \Microservices\models\Model
     public function find($phoneOrEmail,$options = array()) {
         $options = array_merge($options,['limit' => 1]);
         if (filter_var($phoneOrEmail, FILTER_VALIDATE_EMAIL)) {
-            return $this->all(['filter' => ['email' => $phoneOrEmail]],$options);
+            return $this->all(['emailphone' => $phoneOrEmail],$options);
         }
         else {
             $phoneOrEmail = preg_replace('/^0/', '+84', $phoneOrEmail);
-            return $this->all(['filter' => ['phone' => $phoneOrEmail]],$options);
+            return $this->all(['emailphone' => $phoneOrEmail],$options);
         }
     }
 }
