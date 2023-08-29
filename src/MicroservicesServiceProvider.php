0d9cb5530d89a4e7ade9fdda67100c090b837781
Namtq <quocnam@hanoicdc.com>
2023-08-29 19:11:07 +0700

    fix microservice provider

---
+++ src/MicroservicesServiceProvider.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Microservices;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Foundation\AliasLoader;
+use Illuminate\Support\Facades\Event;
+use Microservices\models\Microservices;
+use Microservices\Facade\Microservices as MicroservicesFacade;
+
+class MicroservicesServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // User Provider
+        Event::listen(
+            \Microservices\Events\BusEvent::class,
+            [\Microservices\Listeners\BusListener::class, 'handle']
+        );
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // Facades
+        $this->app->singleton('Microservices', function ($app) {
+            return $app->make(Microservices::class);
+        });
+        $loader = AliasLoader::getInstance();
+        $loader->alias('Microservices', MicroservicesFacade::class);
+        //$this->app->alias('Microservices',MicroservicesFacade::class);
+    }
+}
