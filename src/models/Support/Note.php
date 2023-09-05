76bf93e9a670c70b1d028abdc8ae5ca958dca8ca
hoanguyen004 <hoanguyen004@gmail.com>
2023-09-05 13:59:43 +0700

    Add files via upload

---
+++ src/models/Support/Note.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Microservices\models\Support;
+
+use Illuminate\Support\Arr;
+
+class Note extends \Microservices\models\Model
+{
+    protected $_url;
+    protected $is_cache = 1;
+    public function __construct($options = []) {
+        $this->_url = env('API_MICROSERVICE_URL_V2').'/support/notes';
+        $this->setToken($options['token'] ?? 'system');
+    }
+}
