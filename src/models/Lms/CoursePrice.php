bb4ce001e906c780640b1683801ac8ec4281475f
hoanguyen004 <hoanguyen004@gmail.com>
2023-10-11 16:09:36 +0700

    Add files via upload

---
+++ src/models/Lms/CoursePrice.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Microservices\models\Lms;
+
+use Illuminate\Support\Arr;
+
+class CoursePrice extends \Microservices\models\Model
+{
+    protected $_url;
+    public function __construct($options = []) {
+        $this->_url = env('API_MICROSERVICE_URL_V2').'/lms/course-prices';
+        $this->setToken($options['token'] ?? 'system');
+    }
+}
