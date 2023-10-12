3ed9405a1e2d5e843127d367ccf5e583fd0d8ac2
Namtq <quocnam@hanoicdc.com>
2023-10-12 14:37:44 +0700

    add Event Local

--- src/Events/Event.php
+++ src/Events/Event.php
@@ -7,7 +7,15 @@ class Event
     }
     // public function 
     public function __call($method,$arg = []) {
-        $func = '\Microservices\Events\\'.$method;        
-        return event(new $func(...$arg));
+        $func = '\Microservices\Events\\'.$method;
+        $r = event(new $func(...$arg));
+        //// call event local////  
+        if (class_exists('\App\Events\\'.$arg[0])) {
+            $eventFunction = '\App\Events\\'.$arg[0];
+            $dataEvent = $arg;
+            unset($dataEvent[0]);
+            $r = event(new $eventFunction(...$dataEvent));
+        }
+        return $r;
     }
 }
