30da59432e0abbc5d2a9464859dd2a79d014d4b3
hoanguyen004 <hoanguyen004@gmail.com>
2023-10-18 16:26:01 +0700

    Add files via upload

---
+++ src/models/Test/Test.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Microservices\models\Test;
+
+use Illuminate\Support\Arr;
+
+class Test extends \Microservices\models\Model
+{
+    protected $_url;
+    protected $is_cache = 1;
+    public function __construct($options = []) {
+        $this->_url = env('API_MICROSERVICE_URL_V2').'/tests/tests';
+        $this->setToken($options['token'] ?? 'system');
+    }
+}
