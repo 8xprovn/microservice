0283e932ca6cbe3377432158c5cb1000e161c0d3
hoanguyen004 <hoanguyen004@gmail.com>
2023-10-27 15:03:07 +0700

    Add files via upload

---
+++ src/models/Org/Cities.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Microservices\models\Org;
+
+use Illuminate\Support\Arr;
+
+class Cities extends \Microservices\models\Model
+{
+    protected $_url;
+    protected $is_cache = 1;
+    public function __construct($options = []) {
+        $this->_url = env('API_MICROSERVICE_URL_V2').'/org/city';
+        $this->setToken($options['token'] ?? 'system');
+    }
+}
