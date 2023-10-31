b391e3f47634eb73c10d9d76e67d34affa99e119
Namtq <quocnam@hanoicdc.com>
2023-10-31 09:40:41 +0700

    fix DropboxToken path

--- src/Caches/Uploads/DropboxToken.php
+++ src/Caches/Uploads/DropboxToken.php
@@ -2,7 +2,7 @@
 
 namespace Microservices\Caches\Uploads;
 
-class Brands extends \Microservices\Caches\BaseCache
+class DropboxToken extends \Microservices\Caches\BaseCache
 {
     protected $service = 'uploads';
     protected $table = 'dropbox_token';
