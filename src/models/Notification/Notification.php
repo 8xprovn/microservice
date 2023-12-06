35f469cb807673091381c8e6d5da16326ea752cd
hoanguyen004 <hoanguyen004@gmail.com>
2023-12-06 08:53:31 +0700

    Add files via upload

---
+++ src/models/Notification/Notification.php
@@ -0,0 +1,22 @@
+<?php
+namespace Microservices\models\Notification;
+
+use Illuminate\Support\Facades\Http;
+
+class Notification
+{
+    protected $_url;
+    public function __construct() {
+        $this->_url = 'https://erp-staging.ebomb.edu.vn/notification/api';
+    }
+    public function send($params) {
+        switch($params['type']) {
+            default:
+            $response = \Http::withToken(env('API_MICROSERVICE_TOKEN',''))->post($this->_url.'/type/'.$params['type'],$params);
+            if ($response->successful()) {
+                return true;
+            }
+            return ['message' => $response->body()];
+        }
+    }
+}
