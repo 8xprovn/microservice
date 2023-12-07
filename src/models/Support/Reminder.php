22df4670ef788bdfd6d3e0fe971a961abfd0d120
hoanguyen004 <hoanguyen004@gmail.com>
2023-12-07 16:42:19 +0700

    Update Reminder.php

--- src/models/Support/Reminder.php
+++ src/models/Support/Reminder.php
@@ -12,4 +12,14 @@ class Reminder extends \Microservices\models\Model
         $this->_url = env('API_MICROSERVICE_URL_V2').'/support/reminder';
         $this->setToken($options['token'] ?? 'system');
     }
+
+    public function completed($id)
+    {
+        $response = \Http::acceptJson()->withToken($this->access_token)->post(env('API_MICROSERVICE_URL_V2') . '/support/reminder/'.$id.'/completed');
+        if ($response->successful()) {
+            return $response->json();
+        }
+        \Log::error($this->_url . $response->body());
+        return false;
+    }
 }
