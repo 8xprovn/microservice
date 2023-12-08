8acef9dd58fca78fa8eee1efa0dea0e0e9f56444
hoanguyen004 <hoanguyen004@gmail.com>
2023-12-08 08:44:59 +0700

    Update Reminder.php

--- src/models/Support/Reminder.php
+++ src/models/Support/Reminder.php
@@ -7,7 +7,6 @@ use Illuminate\Support\Arr;
 class Reminder extends \Microservices\models\Model
 {
     protected $_url;
-    protected $is_cache = 1;
     public function __construct($options = []) {
         $this->_url = env('API_MICROSERVICE_URL_V2').'/support/reminder';
         $this->setToken($options['token'] ?? 'system');
