a3994ce6871019a89ce442db72fc2a1fb96de90b
hoanguyen004 <hoanguyen004@gmail.com>
2023-12-18 08:31:51 +0700

    Add files via upload

---
+++ src/models/Hr/EmployeeExtendTemp.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Microservices\models\Hr;
+
+use Illuminate\Support\Arr;
+
+class EmployeeExtendTemp extends \Microservices\models\Model
+{
+    protected $_url;
+    public function __construct($options = []) {
+        $this->_url = env('API_MICROSERVICE_URL_V2').'/hr/employees/extend/temp';
+        $this->setToken($options['token'] ?? 'system');
+    }
+}
