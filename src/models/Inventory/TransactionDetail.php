f4506c67e601a8ff407cb266f62edf2d1fd3279b
hoanguyen004 <hoanguyen004@gmail.com>
2024-01-10 11:22:10 +0700

    Add files via upload

---
+++ src/models/Inventory/TransactionDetail.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Microservices\models\Inventory;
+
+class TransactionDetail extends \Microservices\models\Model
+{
+    protected $_url;
+//    protected $is_cache = 1;
+    public function __construct($options = []) {
+        $this->_url = env('API_MICROSERVICE_URL_V2').'/inventory/transaction_detail';
+        $this->setToken($options['token'] ?? 'system');
+    } 
+}
