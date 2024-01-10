8bd8f62911cb2fdd3a0f0e4183b5d2a86dd821f0
hoanguyen004 <hoanguyen004@gmail.com>
2024-01-10 10:37:50 +0700

    Update Transaction.php

--- src/models/Inventory/Transaction.php
+++ src/models/Inventory/Transaction.php
@@ -10,4 +10,23 @@ class Transaction extends \Microservices\models\Model
         $this->_url = env('API_MICROSERVICE_URL_V2').'/inventory/transaction';
         $this->setToken($options['token'] ?? 'system');
     }
+
+    public function transaction_detail($transaction_id, $options = []) 
+    {
+        $url = env('API_MICROSERVICE_URL_V2')."/inventory/transaction-detail/{$transaction_id}";
+        try { 
+            $response = \Http::acceptJson()->withToken($this->access_token)->get($url);
+           
+            if ($response->successful()) {
+                return $response->json();
+            }
+            if($response->status() == 403){
+                return ['status' => 'error' , 'message'  => 'UnAuthenticated'];
+            }
+            \Log::error($this->_url . $response->body());
+            return [];
+        }catch (\Exception $ex){
+            return [];
+        } 
+    }
 }
