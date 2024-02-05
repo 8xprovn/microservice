d76c22487e04e140dc1f50bfbea3aac8d91622f4
hoanguyen004 <hoanguyen004@gmail.com>
2024-02-05 15:11:00 +0700

    Update Sms.php

--- src/models/Notification/Sms.php
+++ src/models/Notification/Sms.php
@@ -8,7 +8,7 @@ class Sms extends \Microservices\models\Model
 {
     protected $_url;
     public function __construct($options = []) {
-        $this->_url = env('API_MICROSERVICE_URL_V2').'/notification/sms';
+        $this->_url = env('API_MICROSERVICE_URL_V2').'/notification/type/sms';
         $this->setToken($options['token'] ?? 'system');
     }
 }
