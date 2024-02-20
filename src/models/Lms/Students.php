b6615b454d8cc38e8156a40e1b11a3cc4f41129c
hoanguyen004 <hoanguyen004@gmail.com>
2024-02-20 13:36:39 +0700

    Update Students.php

--- src/models/Lms/Students.php
+++ src/models/Lms/Students.php
@@ -11,4 +11,26 @@ class Students extends \Microservices\models\Model
         $this->_url = env('API_MICROSERVICE_URL_V2').'/lms/students';
         $this->setToken($options['token'] ?? 'system');
     }
+
+    public function getContactId($params = [], $options = [])
+    {
+        $filter = [];
+        foreach($params as $k => $v){
+            if (is_null($v)) continue;
+            switch ($k) {
+                default:
+                    $filter[$k] = $v;
+                    break;
+            }
+        }
+        $q = $options;
+        $q['filter'] = $filter;
+        $response = \Http::acceptJson()->withToken($this->access_token)->get($this->_url, $q);
+        if ($response->successful()) {
+            $contact_ids = collect($response->json())->pluck('contact_id')->unique()->values()->all();
+            return $contact_ids;
+        } 
+        \Log::error($this->_url . $response->body());
+        return [];
+    }
 }
