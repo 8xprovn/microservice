03daa971f58b2965c28183e760b608a8f5dbca9f
hoanguyen004 <hoanguyen004@gmail.com>
2024-02-20 13:35:54 +0700

    Update Segments.php

--- src/models/Crm/Segments.php
+++ src/models/Crm/Segments.php
@@ -12,4 +12,14 @@ class Segments extends \Microservices\models\Model
         $this->_url = env('API_MICROSERVICE_URL_V2').'/crm/segments';
         $this->setToken($options['token'] ?? 'system');
     }
+    public function getContacts($params) {
+        $segment = \Microservices::Crm('Segments')->detail($params);
+        if (!empty($segment['data'])) {
+            $contact_ids = collect(\Microservices::Crm('Contacts')->all(json_decode(json_decode($segment['data'])->filter, true), ['limit' => 500]))->pluck('_id')->toArray();
+            if(!empty($contact_ids)) {
+                return array_unique($contact_ids);
+            }
+        }
+        return [];
+    }
 }
