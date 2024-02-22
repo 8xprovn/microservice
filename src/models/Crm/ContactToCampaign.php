a47ee579a5b892ee251e6e1a8a081ace3c89d605
hoanguyen004 <hoanguyen004@gmail.com>
2024-02-22 10:28:23 +0700

    Create ContactToCampaign.php

---
+++ src/models/Crm/ContactToCampaign.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Microservices\models\Crm;
+
+use Illuminate\Support\Arr;
+
+class ContactToCampaign extends \Microservices\models\Model
+{
+    protected $_url;
+    //protected $is_cache = 1;
+    public function __construct($options = []) {
+        $this->_url = env('API_MICROSERVICE_URL_V2').'/crm/contacts-campaign';
+        $this->setToken($options['token'] ?? 'system');
+    }
+    public function getContacts($params) {
+        $contact_ids = \Microservices::Crm('ContactToCampaign')->all(['campaign_id' => $params], ['limit' => 500])->pluck('_id')->toArray();
+        if(!empty($contact_ids)) {
+            return array_unique($contact_ids);
+        }
+        return [];
+    }
+}
