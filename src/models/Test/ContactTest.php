06d4120393134e88ee049f424a7f9b8e12170575
hoanguyen004 <hoanguyen004@gmail.com>
2024-02-23 16:47:56 +0700

    Add files via upload

---
+++ src/models/Test/ContactTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Microservices\models\Test;
+
+use Illuminate\Support\Arr;
+
+class ContactTest extends \Microservices\models\Model
+{
+    protected $_url;
+    public function __construct($options = []) {
+        $this->_url = env('API_MICROSERVICE_URL_V2').'/tests/contact-test';
+        $this->setToken($options['token'] ?? 'system');
+    }
+}
