f96f317b78e826c85e1fb05cff1575c205085080
hoanguyen004 <hoanguyen004@gmail.com>
2024-02-26 09:47:21 +0700

    Add files via upload

---
+++ src/models/Finance/VouchersCategory.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Microservices\models\Finance;
+
+class VouchersCategory extends \Microservices\models\Model
+{
+    protected $_url;
+    public function __construct($options = []) {
+        $this->_url = env('API_MICROSERVICE_URL_V2').'/finance/vouchers-category';
+        $this->setToken($options['token'] ?? 'system');
+    }
+}
