dfa65fe76c88c7783565530ee170b8d5c8905ebf
hoanguyen004 <hoanguyen004@gmail.com>
2024-02-26 09:03:43 +0700

    Update Test.php

--- src/models/Test/Test.php
+++ src/models/Test/Test.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Arr;
 class Test extends \Microservices\models\Model
 {
     protected $_url;
-    protected $is_cache = 1;
+    //protected $is_cache = 1;
     public function __construct($options = []) {
         $this->_url = env('API_MICROSERVICE_URL_V2').'/tests/tests';
         $this->setToken($options['token'] ?? 'system');
