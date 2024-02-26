6d454c0cac01838d0c23415937967917e25db1db
hoanguyen004 <hoanguyen004@gmail.com>
2024-02-26 09:03:52 +0700

    Update TestLog.php

--- src/models/Test/TestLog.php
+++ src/models/Test/TestLog.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Arr;
 class TestLog extends \Microservices\models\Model
 {
     protected $_url;
-    protected $is_cache = 1;
+    //protected $is_cache = 1;
     public function __construct($options = []) {
         $this->_url = env('API_MICROSERVICE_URL_V2').'/tests/test-logs';
         $this->setToken($options['token'] ?? 'system');
