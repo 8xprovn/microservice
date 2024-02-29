9c0084b286d07144ff2345150285b4da0fe7c02e
@hiepcm <hiepcm@imap.edu.vn>
2024-02-29 14:01:11 +0700

    update code

--- src/models/Finance/Vouchers.php
+++ src/models/Finance/Vouchers.php
@@ -5,8 +5,19 @@ namespace Microservices\models\Finance;
 class Vouchers extends \Microservices\models\Model
 {
     protected $_url;
-    public function __construct($options = []) {
-        $this->_url = env('API_MICROSERVICE_URL_V2').'/finance/vouchers';
+    public function __construct($options = [])
+    {
+        $this->_url = env('API_MICROSERVICE_URL_V2') . '/finance/vouchers';
         $this->setToken($options['token'] ?? 'system');
+    }
+    public function checkCode($params = [])
+    {
+        $url = $this->_url . '/code/check';
+        $response = \Http::acceptJson()->withToken($this->access_token)->get($url, $params);
+        if ($response->successful()) {
+            return $response->json();
+        }
+        \Log::error($this->_url . $response->body());
+        return [];
     }
 }
