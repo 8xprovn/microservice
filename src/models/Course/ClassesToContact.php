88341f626716390fc8e5cb2139139329653dc4dc
@hiepcm <hiepcm@imap.edu.vn>
2024-03-21 17:39:07 +0700

    update code

---
+++ src/models/Course/ClassesToContact.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Microservices\models\Course;
+
+use Illuminate\Support\Arr;
+
+class ClassesToContact extends \Microservices\models\Model
+{
+    protected $_url;
+    public function __construct($options = [])
+    {
+        $this->_url = env('API_MICROSERVICE_URL_V2') . '/course/class_contact';
+        $this->setToken($options['token'] ?? 'system');
+    }
+    public function list_classes($params = [], $options = [])
+    {
+        $url = env('API_MICROSERVICE_URL_V2') . '/course/class_to_contact';
+        $filter = [];
+        foreach ($params as $k => $v) {
+            if (is_null($v)) continue;
+            switch ($k) {
+                default:
+                    $filter[$k] = $v;
+                    break;
+            }
+        }
+        $q = $options;
+        $q['filter'] = $filter;
+        $response = \Http::acceptJson()->withToken($this->access_token)->get($url, $q);
+        
+        if ($response->successful()) {
+            return $response->json();
+        }
+        \Log::error($this->_url . $response->body());
+        return [];
+    }
+
+    public function list_courses($params = [], $options = [])
+    {
+        $url = env('API_MICROSERVICE_URL_V2') . '/course/course_to_contact';
+        $filter = [];
+        foreach ($params as $k => $v) {
+            if (is_null($v)) continue;
+            switch ($k) {
+                default:
+                    $filter[$k] = $v;
+                    break;
+            }
+        }
+        $q = $options;
+        $q['filter'] = $filter;
+        $response = \Http::acceptJson()->withToken($this->access_token)->get($url, $q);
+        if ($response->successful()) {
+            return $response->json();
+        }
+        \Log::error($this->_url . $response->body());
+        return [];
+    }
+}
