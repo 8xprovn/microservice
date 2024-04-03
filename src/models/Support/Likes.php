00542a07998de03188366ab82a106b825c5ca263
@hiepcm <hiepcm@imap.edu.vn>
2024-04-03 14:11:54 +0700

    update code like

---
+++ src/models/Support/Likes.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Microservices\models\Support;
+
+class Likes extends \Microservices\models\Model
+{
+    protected $_url;
+    public function __construct($options = [])
+    {
+        $this->_url = env('API_MICROSERVICE_URL_V2') . '/support';
+        $this->setToken($options['token'] ?? 'system');
+    }
+    public function count_like($params)
+    {
+        $_url = $this->_url . "/likes";
+        $response = \Http::acceptJson()->withToken($this->access_token)->get($_url, $params);
+        if ($response->successful()) {
+            return $response->json();
+        } 
+        return 0;
+    }
+    
+    public function change($data)
+    {
+        $_url = $this->_url . "/likes";
+        $response = \Http::acceptJson()->withToken($this->access_token)->post($_url, $data);
+        if ($response->successful()) {
+            return $response->json();
+        } 
+        return 0;
+    }
+}
