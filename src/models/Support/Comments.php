affa3657d6ee2373beb422a680c3cf667db55fc3
@hiepcm <hiepcm@imap.edu.vn>
2024-04-04 16:28:02 +0700

    update render blade comment

--- src/models/Support/Comments.php
+++ src/models/Support/Comments.php
@@ -14,9 +14,20 @@ class Comments extends \Microservices\models\Model
     {
         $_url = env('API_MICROSERVICE_URL_V2') . '/support/comment/count';
         $response = \Http::acceptJson()->withToken($this->access_token)->get($_url, $params);
-        if ($response->successful()) {
+        if ($response->successful()) { 
             return $response->json();
-        } 
+        }
         return 0;
+    }
+
+    public function getComment($filter)
+    {
+        $_url = env('API_MICROSERVICE_URL_V2') . '/support/comment/render_comment';
+        $response = \Http::acceptJson()->withToken($this->access_token)->get($_url, $filter);
+        if ($response->successful()) {
+            $results = $response->json();
+            return $results ?? '';
+        } 
+        return '';
     }
 }
