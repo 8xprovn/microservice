e1f32d0090450152c4a30ecdd0036a4ec3ba8086
@hiepcm <hiepcm@imap.edu.vn>
2024-04-15 16:29:31 +0700

    update code like, comment album

--- src/models/Support/Likes.php
+++ src/models/Support/Likes.php
@@ -7,26 +7,26 @@ class Likes extends \Microservices\models\Model
     protected $_url;
     public function __construct($options = [])
     {
-        $this->_url = env('API_MICROSERVICE_URL_V2') . '/support';
+        $this->_url = env('API_MICROSERVICE_URL_V2') . '/support/likes';
         $this->setToken($options['token'] ?? 'system');
     }
     public function count_like($params)
     {
-        $_url = $this->_url . "/likes";
+        $_url = env('API_MICROSERVICE_URL_V2') . "/support/likes/count";
         $response = \Http::acceptJson()->withToken($this->access_token)->get($_url, $params);
         if ($response->successful()) {
             return $response->json();
-        } 
+        }
         return 0;
     }
-    
+
     public function change($data)
     {
-        $_url = $this->_url . "/likes";
+        $_url = env('API_MICROSERVICE_URL_V2') . "/support/likes";
         $response = \Http::acceptJson()->withToken($this->access_token)->post($_url, $data);
         if ($response->successful()) {
             return $response->json();
-        } 
+        }
         return 0;
     }
 }
