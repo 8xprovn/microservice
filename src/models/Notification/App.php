6db32ecf8cebcd246856182dd9f08752a84cefbf
hoanguyen004 <hoanguyen004@gmail.com>
2024-05-23 09:33:09 +0700

    Update App.php

--- src/models/Notification/App.php
+++ src/models/Notification/App.php
@@ -11,4 +11,21 @@ class App extends \Microservices\models\Model
         $this->_url = env('API_MICROSERVICE_URL_V2').'/notification/type/app';
         $this->setToken($options['token'] ?? 'system');
     }
+
+    public function updateReadStatus($id, $params)
+    {
+        if (!empty($this->only['update'])) {
+            $params = \Arr::only($params, $this->only['update']);
+        }
+        if (!empty($this->idAutoIncrement)) {
+            $id = (int) $id;
+        }
+        $url = env('API_MICROSERVICE_URL_V2') . '/notification/app/is_read/' . $id;
+        $response = \Http::acceptJson()->withToken($this->access_token)->POST($url, $params);
+        if ($response->successful()) {
+            return $response->json();
+        }
+        \Log::error($url . $response->body());
+        return false;
+    }
 }
