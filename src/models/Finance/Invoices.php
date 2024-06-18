4c36a030f704967b2769b761e7563654a4b854c7
hoanguyen004 <hoanguyen004@gmail.com>
2024-06-18 14:05:58 +0700

    Update Invoices.php

--- src/models/Finance/Invoices.php
+++ src/models/Finance/Invoices.php
@@ -11,4 +11,16 @@ class Invoices extends \Microservices\models\Model
     }
     protected $service = 'erp_finance_backend_v2';
     protected $table = 'invoices';
+
+    public function send($id, $param = [])
+    {
+        $url = $this->_url . "/$id/send";
+
+        $response = \Http::acceptJson()->withToken($this->access_token)->post($url, $param);
+        if ($response->successful()) {
+            return $response->json();
+        }
+        \Log::error($this->_url . $response->body());
+        return [];
+    }
 }
