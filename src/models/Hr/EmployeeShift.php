3160ced6033ffb6c6084012c93e5e598fae57691
@hiepcm <hiepcm@imap.edu.vn>
2024-10-11 15:33:34 +0700

    update employee_shift

--- src/models/Hr/EmployeeShift.php
+++ src/models/Hr/EmployeeShift.php
@@ -8,7 +8,7 @@ class EmployeeShift extends \Microservices\models\Model
 {
     protected $_url;
     public function __construct($options = []) {
-        $this->_url = env('API_MICROSERVICE_URL_V2').'/hr/employees/shift';
+        $this->_url = env('API_MICROSERVICE_URL_V2').'/hr/employee_shift';
         $this->setToken($options['token'] ?? 'system');
     }
 }
