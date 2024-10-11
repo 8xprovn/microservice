ee56fd548c94473e3852f2d9838fd9587fbd4b88
hoanguyen004 <hoanguyen004@gmail.com>
2024-10-11 15:42:54 +0700

    Add files via upload

---
+++ src/models/Integration/Qr.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Microservices\models\Integration;
+
+
+class Qr
+{
+    private $md5Secret = 'Imap_123@';
+    public function __construct($options = [])
+    {
+    }
+
+    function create($string, $size = 250)
+    {
+        $_url = env('URL_QR') ?? 'https://qr.ebomb.edu.vn/file/qr';
+        $file = '';
+        if (empty($string)) {
+            return $file;
+        }
+        $namefile = md5($string.$this->md5Secret).'.png';
+        $file = $_url.'/'.$size.'/'.$namefile.'?string='.urlencode($string);
+        return $file;
+    }
+}
