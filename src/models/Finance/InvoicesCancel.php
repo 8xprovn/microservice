1aa06d534466358a146fd70b6950e03cae56304e
hoanguyen004 <hoanguyen004@gmail.com>
2024-11-04 15:17:11 +0700

    Update InvoicesCancel.php

--- src/models/Finance/InvoicesCancel.php
+++ src/models/Finance/InvoicesCancel.php
@@ -2,25 +2,11 @@
 
 namespace Microservices\models\Finance;
 
-class Invoices extends \Microservices\models\Model
+class InvoicesCancel extends \Microservices\models\Model
 {
     protected $_url;
     public function __construct($options = []) {
-        $this->_url = env('API_MICROSERVICE_URL_V2').'/finance/invoices';
+        $this->_url = env('API_MICROSERVICE_URL_V2').'/finance/invoices-cancel';
         $this->setToken($options['token'] ?? 'system');
-    }
-    protected $service = 'erp_finance_backend_v2';
-    protected $table = 'invoices';
-
-    public function send($id, $param = [])
-    {
-        $url = $this->_url . "/$id/send";
-
-        $response = \Http::acceptJson()->withToken($this->access_token)->post($url, $param);
-        if ($response->successful()) {
-            return $response->json();
-        }
-        \Log::error($this->_url . $response->body());
-        return [];
     }
 }
