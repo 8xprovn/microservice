8b18cc31b44eec61f72abe32d6333cc778662014
hoanguyen004 <hoanguyen004@gmail.com>
2024-11-04 11:05:28 +0700

    Add files via upload

---
+++ src/models/Finance/InvoicesLogsAssign.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Microservices\models\Finance;
+
+class InvoicesLogsAssign extends \Microservices\models\Model
+{
+    protected $_url;
+    public function __construct($options = []) {
+        $this->_url = env('API_MICROSERVICE_URL_V2').'/finance/invoices-logs-assign';
+        $this->setToken($options['token'] ?? 'system');
+    }
+}
