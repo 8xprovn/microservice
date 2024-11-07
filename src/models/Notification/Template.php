b23b9b4382805159a4e5eda0313de22259509c31
hoanguyen004 <hoanguyen004@gmail.com>
2024-11-07 11:54:09 +0700

    Add files via upload

---
+++ src/models/Notification/Template.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Microservices\models\Notification;
+
+use Illuminate\Support\Arr;
+
+class Template extends \Microservices\models\Model
+{
+    protected $_url;
+    public function __construct($options = []) {
+        $this->_url = env('API_MICROSERVICE_URL_V2').'/notification/template';
+        $this->setToken($options['token'] ?? 'system');
+    }
+}
