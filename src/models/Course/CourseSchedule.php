3e78430710dc1587bf92945cdf3438c146a1e7b4
hoanguyen004 <hoanguyen004@gmail.com>
2024-12-16 14:16:09 +0700

    Add files via upload

---
+++ src/models/Course/CourseSchedule.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Microservices\models\Course;
+
+use Illuminate\Support\Arr;
+
+class CourseSchedule extends \Microservices\models\Model
+{
+    protected $_url;
+    public function __construct($options = []) {
+        $this->_url = env('API_MICROSERVICE_URL_V2').'/course/course_schedule';
+        $this->setToken($options['token'] ?? 'system');
+    }
+}
