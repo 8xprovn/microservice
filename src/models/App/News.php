50428323f7d8e30d51bd887173dd40912983e378
hoanguyen004 <hoanguyen004@gmail.com>
2025-01-07 16:59:27 +0700

    Update News.php

--- src/models/App/News.php
+++ src/models/App/News.php
@@ -11,4 +11,27 @@ class News extends \Microservices\models\Model
         $this->_url = env('API_MICROSERVICE_URL_V2').'/app/news';
         $this->setToken($options['token'] ?? 'system');
     }
+
+    public function count($params = [], $options = [])
+    {
+        $filter = [];
+        foreach($params as $k => $v){
+            if (is_null($v)) continue;
+            switch ($k) {
+                default:
+                    $filter[$k] = $v;
+                    break;
+            }
+        }
+        $q = $options;
+        $q['filter'] = $filter;
+        $response = \Http::acceptJson()
+                    ->withToken($this->access_token)
+                    ->get($this->_url . '/count', $q);
+        if ($response->successful()) {
+            return $response->json();
+        } 
+        \Log::error($this->_url . $response->body());
+        return false;
+    }
 }
