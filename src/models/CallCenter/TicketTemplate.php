e0b60acbc488589fbc248792825265934a79603e
hoanguyen004 <hoanguyen004@gmail.com>
2025-01-14 13:53:24 +0700

    Create TicketTemplate.php

---
+++ src/models/CallCenter/TicketTemplate.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Microservices\models\CallCenter;
+
+class TicketTemplate extends \Microservices\models\Model
+{
+    protected $_url;
+    public function __construct($options = []) {
+        $this->_url = env('API_MICROSERVICE_URL_V2').'/call-center/ticket-template';
+        $this->setToken($options['token'] ?? 'system');
+    }
+}
