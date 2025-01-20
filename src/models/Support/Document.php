927c096d4d4837c0b7702ee3912817b329a2f9c8
thang nd <thangnd1@imap.edu.vn>
2025-01-20 15:37:42 +0700

    add document support

--- src/models/Support/Document.php
+++ src/models/Support/Document.php
@@ -8,6 +8,5 @@ class Document extends \Microservices\models\Model
     protected $_url;
     public function __construct($options = []) {
         $this->_url = env('API_MICROSERVICE_URL_V2').'/support/document';
-        $this->setToken($options['token'] ?? 'system');
     }
 }
--- src/models/Support/SettingDocument.php
+++ src/models/Support/SettingDocument.php
@@ -8,6 +8,5 @@ class SettingDocument extends \Microservices\models\Model
     protected $_url;
     public function __construct($options = []) {
         $this->_url = env('API_MICROSERVICE_URL_V2').'/support/setting_document';
-        $this->setToken($options['token'] ?? 'system');
     }
 }
