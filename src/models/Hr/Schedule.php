779ba81e5bdf6473d05a0fd43f38c2e662ee4fdd
@hiepcm <hiepcm@imap.edu.vn>
2025-02-13 10:20:29 +0700

    update schedule holiday to employee

--- src/models/Hr/Schedule.php
+++ src/models/Hr/Schedule.php
@@ -7,8 +7,18 @@ use Illuminate\Support\Arr;
 class Schedule extends \Microservices\models\Model
 {
     protected $_url;
-    public function __construct($options = []) {
-        $this->_url = env('API_MICROSERVICE_URL_V2').'/hr/schedule';
-        
+    public function __construct($options = [])
+    {
+        $this->_url = env('API_MICROSERVICE_URL_V2') . '/hr/schedule';
+    }
+    public function getHolidaytoEmployee($employee_id, $month, $year)
+    {
+        $params = ['employee_id' => $employee_id, 'month' => $month, 'year' => $year]; 
+        $response = \Http::acceptJson()->withToken($this->getToken())->get("{$this->_url}/holiday", $params);
+        if ($response->successful()) {
+            return $response->json();
+        }
+        \Log::error("{$this->_url}/holiday" . $response->body());
+        return [];
     }
 }
