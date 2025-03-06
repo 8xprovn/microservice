2ae76c484c483e863c1fce73ffa83f869d8c3b82
@hiepcm <hiepcm@imap.edu.vn>
2025-03-06 17:07:56 +0700

    update security

--- src/models/Security/Url.php
+++ src/models/Security/Url.php
@@ -7,7 +7,9 @@ class Url
     public function md5_code($string, $config_key_md5 = '')
     {
         if (empty($config_key_md5)) $config_key_md5 = env('SECURITY_VIEW_URL_MD5');
-        if (is_array($string)) $string = implode('-', $string);
+        if (is_array($string)){
+            $string = implode('-', $string);
+        }
         return md5("{$config_key_md5}_{$string}");
     }
 }
