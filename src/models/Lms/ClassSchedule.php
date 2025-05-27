962322c09791e86433615e3e92c5bf8341688efa
thang nd <thangnd1@imap.edu.vn>
2025-05-27 10:20:57 +0700

    update lms semesterTest

--- src/models/Lms/ClassSchedule.php
+++ src/models/Lms/ClassSchedule.php
@@ -11,4 +11,33 @@ class ClassSchedule extends \Microservices\models\Model
         $this->_url = env('API_MICROSERVICE_URL_V2').'/lms/class-schedules';
         
     }
+
+    public function semesterTest($id, $params)
+    {
+        $url = $this->_url . '/' . $id . '/semester-test';
+        $response = \Http::acceptJson()->withToken($this->access_token)->POST($url, $params);
+        if ($response->successful()) {
+            return $response->json();
+        }
+        $responseJson = $response->json();
+        if (isset($responseJson['message'])) {
+            return $responseJson;
+        }
+        \Log::error($this->_url . $response->body());
+        return [];
+    }
+    public function cancelSemesterTest($id, $params)
+    {
+        $url = $this->_url . '/' . $id . '/cancel-semester-test';
+        $response = \Http::acceptJson()->withToken($this->access_token)->POST($url, $params);
+        if ($response->successful()) {
+            return $response->json();
+        }
+        $responseJson = $response->json();
+        if (isset($responseJson['message'])) {
+            return $responseJson;
+        }
+        \Log::error($this->_url . $response->body());
+        return [];
+    }
 }
