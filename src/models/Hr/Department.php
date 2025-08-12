ee64f8ebb27a305606025ba284bafa50691f7e2b
@hiepcm <hiepcm@imap.edu.vn>
2025-08-12 15:17:56 +0700

    update cache department and job_title

--- src/models/Hr/Department.php
+++ src/models/Hr/Department.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Arr;
 class Department extends \Microservices\models\Model
 {
     protected $_url;
+     protected $is_cache = 1;
     public function __construct($options = []) {
         $this->_url = env('API_MICROSERVICE_URL_V2').'/hr/departments';
         
--- src/models/Hr/JobTitle.php
+++ src/models/Hr/JobTitle.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Arr;
 class JobTitle extends \Microservices\models\Model
 {
     protected $_url;
+     protected $is_cache = 1;
     public function __construct($options = []) {
         $this->_url = env('API_MICROSERVICE_URL_V2').'/hr/setting/job_title';
         
