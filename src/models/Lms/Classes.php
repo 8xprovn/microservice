3db790de139fc2efe6a246ee0376ba016c92c20d
hoanguyen004 <hoanguyen004@gmail.com>
2025-08-12 15:22:02 +0700

    Update Classes.php

--- src/models/Lms/Classes.php
+++ src/models/Lms/Classes.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Arr;
 class Classes extends \Microservices\models\Model
 {
     protected $_url;
+    protected $is_cache = 1;
     public function __construct($options = []) {
         $this->_url = env('API_MICROSERVICE_URL_V2').'/lms/classes';
         
