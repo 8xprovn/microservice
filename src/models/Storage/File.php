5565749665dc5e022d822ca4bd7d609ffab17f07
@hiepcm <hiepcm@imap.edu.vn>
2025-10-13 14:35:34 +0700

    update code channel view file

--- src/models/Storage/File.php
+++ src/models/Storage/File.php
@@ -35,19 +35,15 @@ class File
 
     public function show($path, $params = [])
     {
-        $pathTrim = trim($path, '/');
-        $arrs = explode('/', $pathTrim);
-
+        $arrs = explode('/', trim($path, '/'));
         if (empty($arrs)) return '/';
-        $channel = env('UPLOAD_CHANNEL', '');
-        $configDomainChannel = config("storage.{$channel}");
-        $diskUpload = env('UPLOAD_DISK', trim($arrs[0] ?? ''));
+        $channel = env('UPLOAD_CHANNEL', trim($arrs[0] ?? ''));
+        $configChannel = config("storage.{$channel}");
 
-        if (empty($configDomainChannel)) return env('SERVICE_MEDIA_URL', '') . '/' . trim($path, '/');
-        $disk = $configDomainChannel[$diskUpload] ?? [];
+        if (empty($configChannel)) return env('SERVICE_MEDIA_URL', '') . '/' . trim($path, '/');
 
-        $configDomain = $disk['url'] ?? env('SERVICE_MEDIA_URL', '');
-        $configDriver = $disk['driver'] ?? '';
+        $configDomain = $configChannel['url'] ?? env('SERVICE_MEDIA_URL', '');
+        $configDriver = $configChannel['driver'] ?? '';
 
         switch ($configDriver) {
             case "onedrive":
