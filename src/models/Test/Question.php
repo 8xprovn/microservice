8814e1d0dda3b82b862087e80fa705a236a8af7c
@hiepcm <hiepcm@imap.edu.vn>
2025-11-26 15:41:20 +0700

    update cache test

--- src/models/Test/Question.php
+++ src/models/Test/Question.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Arr;
 class Question extends \Microservices\models\Model
 {
     protected $_url;
+    protected $is_cache = 1;
     public function __construct($options = []) {
         $this->_url = env('API_MICROSERVICE_URL_V2').'/tests/questions';
         
