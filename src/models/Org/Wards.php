61acdbd0b17266720056305bbbf83d1c3f8e3523
thang nd <thangnd1@imap.edu.vn>
2026-01-06 10:33:16 +0700

    add org ward and province

--- src/models/Org/Wards.php
+++ src/models/Org/Wards.php
@@ -9,6 +9,5 @@ class Wards extends \Microservices\models\Model
     protected $_url;
     public function __construct($options = []) {
         $this->_url = env('API_MICROSERVICE_URL_V2').'/org/ward';
-        
     }
 }
