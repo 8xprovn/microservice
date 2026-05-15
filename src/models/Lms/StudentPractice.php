aeeb84a2083c968b59fa319e1958a0f5267b77ce
thang nd <thangnd1@imap.edu.vn>
2026-05-15 10:34:12 +0700

    add student relearn

--- src/models/Lms/StudentPractice.php
+++ src/models/Lms/StudentPractice.php
@@ -8,7 +8,7 @@ class StudentRelearn extends \Microservices\models\Model
 {
     protected $_url;
     public function __construct($options = []) {
-        $this->_url = env('API_MICROSERVICE_URL_V2').'/lms/students-relearn';
+        $this->_url = env('API_MICROSERVICE_URL_V2').'/lms/students-practice';
         
     }
 }
--- src/models/Lms/StudentRelearn.php
+++ src/models/Lms/StudentRelearn.php
@@ -8,7 +8,7 @@ class StudentPractice extends \Microservices\models\Model
 {
     protected $_url;
     public function __construct($options = []) {
-        $this->_url = env('API_MICROSERVICE_URL_V2').'/lms/students-practice';
+        $this->_url = env('API_MICROSERVICE_URL_V2').'/lms/students-relearn';
         
     }
 }
