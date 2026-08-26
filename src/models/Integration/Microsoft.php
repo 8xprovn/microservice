ed0f831d422009d34db1ab8302402a2fee4a7b49
Chúc Quân <quanca@imap.edu.vn>
2026-08-26 15:36:52 +0700

    update get access token microsoft

--- src/models/Integration/Microsoft.php
+++ src/models/Integration/Microsoft.php
@@ -11,31 +11,44 @@ class Microsoft
 
     }
 
-    public function getAccessToken($params = [])
+    public function getAccessToken($delegated = false, $forceRefresh = false)
     {
-        ///////// SETTING //////
-        $tenantId = env('MICROSOFT_TENANT_ID');'';
-        $clientId = env('MICROSOFT_CLIENT_ID');'';
-        $clientSecret = env('MICROSOFT_CLIENT_SECRET');'';
-        /////// CACHE KEY /////
-        $cacheKey = 'microsoft_access_token_'.$clientId;
-        ///// CACHE HERE /////
-        $token = \Cache::get($cacheKey);
-        if ($token) {
-            return $token;
-        }
-        //// IF NOT ///////
-        $url = 'https://login.microsoftonline.com/' . $tenantId . '/oauth2/v2.0/token';
+        $tenantId = env('MICROSOFT_TENANT_ID');
+        $clientId = env('MICROSOFT_CLIENT_ID');
+        $clientSecret = env('MICROSOFT_CLIENT_SECRET');
+        $cacheKey = $delegated
+            ? 'microsoft_delegated_token_'.$clientId
+            : 'microsoft_access_token_'.$clientId;
+        $cacheSeconds = $delegated ? 3000 : 600;
 
-        $response = \Http::asForm()->post($url,[
-                    'client_id' => $clientId,
-                    'client_secret' => $clientSecret,
-                    'scope' => 'https://graph.microsoft.com/.default',
-                    'grant_type' => 'client_credentials',
-        ]);
+        if (!$forceRefresh) {
+            $token = \Cache::get($cacheKey);
+            if ($token) {
+                return $token;
+            }
+        }
+
+        $params = [
+            'client_id' => $clientId,
+            'client_secret' => $clientSecret,
+            'scope' => 'https://graph.microsoft.com/.default',
+            'grant_type' => 'client_credentials',
+        ];
+        if ($delegated) {
+            $params = array_merge($params, [
+                'grant_type' => 'password',
+                'username' => env('MICROSOFT_SENDER_UPN'),
+                'password' => env('MICROSOFT_SENDER_PASSWORD'),
+            ]);
+        }
+
+        $response = \Http::asForm()->post(
+            'https://login.microsoftonline.com/' . $tenantId . '/oauth2/v2.0/token',
+            $params
+        );
         $token = $response->json();
         if (!empty($token['access_token'])) {
-            \Cache::put($cacheKey, $token['access_token'], 600);    
+            \Cache::put($cacheKey, $token['access_token'], $cacheSeconds);
             return $token['access_token'];
         }
         return false;
