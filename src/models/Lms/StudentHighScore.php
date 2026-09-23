5d28e3e66d50ce4596a40c782d1ef173a1bc876b
thang nd <thangnd1@imap.edu.vn>
2026-09-23 13:40:22 +0700

    update StudentHighScore

--- src/models/Lms/StudentHighScore.php
+++ src/models/Lms/StudentHighScore.php
@@ -8,7 +8,7 @@ class StudentHighScore extends \Microservices\models\Model
 {
     protected $_url;
     public function __construct($options = []) {
-        $this->_url = env('API_MICROSERVICE_URL_V2').'/lms/highscore';
+        $this->_url = env('API_MICROSERVICE_URL_V2').'/lms/student/highscore';
         
     }
 
