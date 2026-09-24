ac580f26b63bba9b2fe03cf88a10db5cdda11749
@hiepcm <hiepcm@imap.edu.vn>
2026-09-24 17:45:35 +0700

    update schedule holiday

--- src/models/Hr/Schedule.php
+++ src/models/Hr/Schedule.php
@@ -14,12 +14,8 @@ class Schedule extends \Microservices\models\Model
     public function getHolidaytoEmployee($employee_id, $month, $year)
     {
         $params = ['employee_id' => $employee_id, 'month' => $month, 'year' => $year];
-        $response = \Http::acceptJson()->withToken($this->getToken())->get("{$this->_url}/holiday", $params);
-        if ($response->successful()) {
-            return $response->json();
-        }
-        \Log::error("{$this->_url}/holiday" . $response->body());
-        return [];
+        $accessToken = $this->getToken();
+        return $this->safeGet("{$this->_url}/holiday", $params, $accessToken);
     }
     public function getScheduleMe($userId, $key_cache, $params)
     {
